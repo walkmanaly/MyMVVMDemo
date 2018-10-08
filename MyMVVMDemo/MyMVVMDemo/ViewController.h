4c49b2239cf3581f2da045f59deee825773dd586
Nick <walkmanaly@gmail.com>
2018-10-08 11:15:34 +0800

    initializer
    initilaizer project

---
+++ MyMVVMDemo/MyMVVMDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyMVVMDemo
+//
+//  Created by Nick on 2018/10/8.
+//  Copyright © 2018 Nick. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
